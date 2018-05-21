e5e7f2c4177df054013c7faaee387dff3d596f55
drake.aren <D.aren.nalson@gmail.com>
2018-05-21 16:08:22 +0600

    added base scene system

--- src/engine/core.clj
+++ src/engine/core.clj
@@ -9,20 +9,25 @@
             [engine.input.mouse :as mouse]
             [engine.resource.manager :as resource-manager]))
 
-(defonce global
-  (atom
-   {:errorCallback nil
-    :keyCallback nil
-    :window nil
-    :window-height 0
-    :window-width 0
-    :window-title "none"
-    :delta-time 0
-    :main-loop (fn [g])
-    :mouse-buffer
-    {:x (BufferUtils/createDoubleBuffer 1)
-     :y (BufferUtils/createDoubleBuffer 1)}
-    :resources nil}))
+(defonce global-template
+  ^{:doc "Field description: 
+    :scene - used for storage scene for game
+    :resources - used to store repeated resources, that was added from scene"}
+  {:errorCallback nil
+   :keyCallback nil
+   :window
+   {:object nil
+    :height 0
+    :width 0}
+   :title "none"
+   :delta-time 0
+   :current-scene nil
+   :scenes nil
+   :resources nil
+   :main-loop nil
+   :mouse-buffer
+   {:x (BufferUtils/createDoubleBuffer 1)
+    :y (BufferUtils/createDoubleBuffer 1)}})
 
 (defn init
   [context]
@@ -34,31 +39,29 @@
   ;   (s/optional-kel :title) s/Str}
   ;  context)
   (let [{remote-window :remote-window
-         title :title  
+         title :title
          height :height
          width :width
          processor :processor
          resources :resources} context]
-    (swap! global assoc
-          :main-loop processor
-          :title title)))
-
-(defn main-loop [] ((:main-loop @global) global))
+    (-> global-template
+        (assoc :main-loop processor)
+        (assoc :title title)
+        (atom))))
 
 (defn loop!
-  []
+  [global]
   (window/init-windowed global 500 500 "test")
   (render/init global)
   (while (not (GLFW/glfwWindowShouldClose (:window @global)))
     (mouse/update-mouse-position global)
-    (main-loop)
+    ((:main-loop @global) global)
     (render/rerender global)
-    (render/update-delta-time global)
     ; update event pool
     (GLFW/glfwPollEvents)))
 
 (defn stop
-  []
+  [global]
   (.free (:keyCallback @global))
   (.free (:errorCallback @global))
   (window/close @global))
---
+++ src/engine/game_play/scene.clj
@@ -0,0 +1,32 @@
+(ns engine.game-play.scene)
+
+(defonce scene-template
+  {:resources nil
+   :loop-fn nil})
+
+(defn add-scene
+  [global scene-name scene-object]
+  (swap! global
+         assoc-in [:scenes (keyword scene-name)]
+         scene-object))
+
+(defn remove-scene
+  [global scene-name]
+  (swap! global
+         dissoc
+         (:scenes @global) (keyword scene-name)))
+
+(defmacro new-scene
+  [resources loop-fn]
+  (-> scene-template
+      (assoc :resources resources)
+      (assoc :loop-fn loop-fn)))
+
+(defn change-scene
+  [global scene-name]
+  (let [next-scene (get (:scenes @global) (keyword scene-name))]
+    (when (nil? next-scene)
+      (throw (Exception. "Scene not found")))
+    (swap! global
+           assoc :current-scene
+           (keyword scene-name))))
